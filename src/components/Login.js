32e0417aed675bbac73f4621e6e82b3e160a2c18
rskrishnu <2020bmm1009@iitrpr.ac.in>
2022-05-22 09:35:50 +0530

    test

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,7 @@ import {useAuth} from '../contexts/AuthContext'
 import GoogleButton from "react-google-button";
 
 export default function Login(){
-    const emailRef = useRef()
+    
     const passwordRef= useRef()
     const {login,googleSignIn} = useAuth()
     const [error, setError] = useState('')
